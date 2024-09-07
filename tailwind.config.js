582c4d5930673de36f4c4ddf3270d017dc42206f
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2024-09-07 13:01:42 +0700

    Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./public/**/*.html", "./public/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
